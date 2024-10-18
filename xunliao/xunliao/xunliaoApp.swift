467ef99b49f01a6f8959cc91956b241fb73360d7
猿大侠 <484175472@qq.com>
2024-10-18 17:35:10 +0800

    create app

---
+++ xunliao/xunliao/xunliaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  xunliaoApp.swift
+//  xunliao
+//
+//  Created by  mac on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct xunliaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
